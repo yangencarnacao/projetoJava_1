7729c67e644a044ff5efdc8e6cd272e8f518ebb7
Yang Encarnação <yang.silva@gmail.com>
2021-09-12 08:04:15 -0300

    primeiro commit

---
+++ src/curso/java/alex/Pessoa.java
@@ -0,0 +1,25 @@
+package curso.java.alex;
+
+public class Pessoa {
+	
+	private String name;
+	
+	private String sobrenome;
+	
+	private int idade; {
+		return idade;
+	}
+
+	public void SetIdade(int idade) {
+		this.idade = idade;
+		
+		}
+	
+	public String getName() {
+		
+	
+		
+	
+	
+
+}
